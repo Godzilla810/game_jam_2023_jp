5292a62183b3e24fdb95140915039449067d6470
alex921024 <107916257+alex921024@users.noreply.github.com>
2023-07-18 19:19:35 +0800

    4.3.4
    player heart

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,39 +9,38 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody2D enemyRb;
     private GameObject player;
+    private PlayerStatus playerStatus;
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");
+        playerStatus = PlayerStatus.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null && !BtnForEnd.instance.isPause){
+        if (player != null && !BtnForEnd.instance.isPause)
+        {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-            if (distanceToPlayer <= attackRange){
+            if (distanceToPlayer <= attackRange)
+            {
                 Vector3 lookDirection = (player.transform.position - transform.position).normalized;
                 transform.Translate(lookDirection * speed * Time.deltaTime);
             }
         }
     }
-    void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.tag == "Player")
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
         {
-            Destroy(other.gameObject);
-            Restart();
+            playerStatus.Damage();
         }
     }
     void OnDrawGizmosSelected()     //在Scene視圖中繪製調試或可視化信息的圖形元素
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
-    }
-
-    public void Restart(){
-        Debug.Log("Restart");
-        SceneManager.LoadScene(05);
     }
 }
--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,29 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStatus : MonoBehaviour
 {
-    public int HP;
+    public static PlayerStatus instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+
+    public int lives;
+    public GameObject[] hearts;
     // Start is called before the first frame update
     void Start()
     {
-        HP = 100;
+        lives = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void TakeDamage(int damage){
-        HP -= damage;
-        Debug.Log("-10");
-        if (HP <= 0){
+    public void Damage()
+    {
+        lives -= 1;
+        hearts[lives].SetActive(false);
+        Debug.Log("-1");
+        if (lives <= 0)
+        {
             PlayerDie();
         }
     }
-    void PlayerDie(){
+    void PlayerDie()
+    {
         Destroy(gameObject);
+        Restart();
+    }
+    public void Restart()
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene(05);
     }
 }
